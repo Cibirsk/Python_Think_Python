f18924c5655bcce6f8eded43230b3c419de602fc
Cibirsk <stlv@gmx.com>
2022-04-05 18:47:33 +0200

    Update ex_4_1.py

--- chap_3/ex_4_1.py
+++ chap_3/ex_4_1.py
@@ -1,28 +1,7 @@
-"""This module contains a code example related to
-
-Think Python, 2nd Edition
-by Allen Downey
-http://thinkpython2.com
-
-Copyright 2015 Allen Downey
-
-License: http://creativecommons.org/licenses/by/4.0/
-"""
-
 from __future__ import print_function, division
 
 import math
 import turtle
-
-
-def square(t, length):
-    """Draws a square with sides of the given length.
-
-    Returns the Turtle to the starting position and location.
-    """
-    for i in range(4):
-        t.fd(length)
-        t.lt(90)
 
 
 def polyline(t, n, length, angle):
