e4bc6ab3e7a6558ac8d9fca810f5c9c6248cb802
Cibirsk <stlv@gmx.com>
2022-05-03 13:44:41 +0200

    Update ex_9_5.py

--- ex_9_5.py
+++ ex_9_5.py
@@ -2,3 +2,12 @@
 # and a string of required letters,
 # and that returns True if the word uses all the required letters at least once. 
 # How many words are there that use all the vowels aeiou? How about aeiouy?
+
+def uses_all(word, required):
+    for letter in required:
+        if letter not in word:
+            return False
+    return True
+
+
+print(uses_all("papavs", "apsv"))
